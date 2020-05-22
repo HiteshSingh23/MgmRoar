d656bd21af494a1a5a94da97234cc1b26a8690ce
Ashish Rawat <ashish.rawat@berkeleylights.com>
2020-05-22 10:43:51 +0530

    changes in conftest

--- tests/test_listdetails.py
+++ tests/test_listdetails.py
@@ -18,58 +18,58 @@ def test_loginpage(browser):
     assert verify == "Log in to MGM ROAR", "Unable to load login page"
     print(verify)
 
-# @allure.title('Verify carousel image set by admin is visible')
-# def test_verifycarousalimage(browser):
-#     verify = user.CarouselImage()
-#     assert verify == True
-#
-#
-# @allure.title('verify elements present in login model')
-# def test_elementloginmodel(browser):
-#     email = user.VerifyEmail()
-#     assert email == True, "Email textbox not found"
-#     next_button = user.VerifyNextButton()
-#     assert next_button == True, "Next button not found"
-#     # request_account = user.VerifyRequestAnAccount()
-#     # assert request_account == True, "Request an account button not found"
-#
-#
-# @allure.title('Verify user is able to enter Email in email field')
-# def test_enteremail(browser):
-#     user.EnterEmail(email)
-#
-#
-# @allure.title('Verify user with valid email id can proceed to next step')
-# def test_clicknext(browser):
-#     verify = user.ClickNext()
-#     assert verify == True, "user not proceed to next step with valid email id"
-#
-#
-# @allure.title('verify user is successfully logged in')
-# def test_login(browser):
-#     user.EnterPassword(password)
-#     user.ClickNext()
-#     time.sleep(10)
-#     verify = user.VerifyLogin()
-#     assert verify == True, "User is not able to login with valid credentials"
-#
-#
-# @allure.title('tc-35,Verify elements in mylist details page')
-# def test_mylistdetails(browser):
-#     global listdet
-#     listdet = ListDetails(browser)
-#     listdet.verify_addmovietolist()
-#     listdet.verify_mylist()
-#     assert listdet.verify_listheadername() == 'movietest', "title not found in list details page"
-#     assert listdet.verify_listgridbutton() == True, "grid button not found in list details page"
-#     assert listdet.verify_listbutton() == True, "list button not found in list details page"
-#     assert listdet.verify_sharebutton() == True, "share button not found in list details page"
-#     assert listdet.verify_deletebutton() == True, "delete button not found in list details page"
-#     assert listdet.verify_moviedetail() == True, "movie details not found in list details page"
-#     assert listdet.verify_moviecheckbox() == True, "movie checkbox not found in list details page"
-#     # assert listdet.verify_listcheckbox() == True, "checkbox button not found in list details page"
+@allure.title('Verify carousel image set by admin is visible')
+def test_verifycarousalimage(browser):
+    verify = user.CarouselImage()
+    assert verify == True
 
-#
+
+@allure.title('verify elements present in login model')
+def test_elementloginmodel(browser):
+    email = user.VerifyEmail()
+    assert email == True, "Email textbox not found"
+    next_button = user.VerifyNextButton()
+    assert next_button == True, "Next button not found"
+    # request_account = user.VerifyRequestAnAccount()
+    # assert request_account == True, "Request an account button not found"
+
+
+@allure.title('Verify user is able to enter Email in email field')
+def test_enteremail(browser):
+    user.EnterEmail(email)
+
+
+@allure.title('Verify user with valid email id can proceed to next step')
+def test_clicknext(browser):
+    verify = user.ClickNext()
+    assert verify == True, "user not proceed to next step with valid email id"
+
+
+@allure.title('verify user is successfully logged in')
+def test_login(browser):
+    user.EnterPassword(password)
+    user.ClickNext()
+    time.sleep(10)
+    verify = user.VerifyLogin()
+    assert verify == True, "User is not able to login with valid credentials"
+
+
+@allure.title('tc-35,Verify elements in mylist details page')
+def test_mylistdetails(browser):
+    global listdet
+    listdet = ListDetails(browser)
+    listdet.verify_addmovietolist()
+    listdet.verify_mylist()
+    assert listdet.verify_listheadername() == 'movietest', "title not found in list details page"
+    assert listdet.verify_listgridbutton() == True, "grid button not found in list details page"
+    assert listdet.verify_listbutton() == True, "list button not found in list details page"
+    assert listdet.verify_sharebutton() == True, "share button not found in list details page"
+    assert listdet.verify_deletebutton() == True, "delete button not found in list details page"
+    assert listdet.verify_moviedetail() == True, "movie details not found in list details page"
+    assert listdet.verify_moviecheckbox() == True, "movie checkbox not found in list details page"
+    # assert listdet.verify_listcheckbox() == True, "checkbox button not found in list details page"
+
+
 # @allure.title('tc-36,Verify overlay present in movie card')
 # def test_mylistoverlaydetails(browser):
 #     global listdet
